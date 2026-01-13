11adaf8521653c20b38b64377c7b5d968e2875b3
connoraxtell-mines <connor.axtell@mines.sdsmt.edu>
2026-01-13 12:43:22 -0700

    first commit

---
+++ exampleProject/src/main.cpp
@@ -0,0 +1,9 @@
+#include "test.h"
+
+#include <iostream>
+
+int main() {
+    testFunction();
+    std::cout << "Library linked and tested successfully!" << std::endl;
+    return 0;
+}
---
+++ src/test.cpp
@@ -0,0 +1,8 @@
+#include "test.h"
+
+#include <iostream>
+
+void testFunction() {
+    // Implementation of the test function
+    std::cout << "Test function executed." << std::endl;
+}
